3ba4942f5ad823c6a31c81a98b89ace7fa4a1e5e
nino <garo_n@alternance.net>
2022-08-10 22:00:39 +0200

    in progress, add components to config

--- frontend/components/CompTable.tsx
+++ frontend/components/CompTable.tsx
@@ -1,8 +1,9 @@
+import { cp } from "fs/promises";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import { showComponents } from "../stores/config/config";
+import { showComponents, updateConfig } from "../stores/config/config";
 import { RootState } from "../stores/config/useConfig";
-import { IComponent } from "../types/computerTypes";
+import { IComponent, IConfig } from "../types/computerTypes";
 
 const CompTable = ({
   components,
@@ -13,6 +14,48 @@ const CompTable = ({
 }) => {
   const config = useSelector((state: RootState) => state.config);
   const dispatch = useDispatch();
+
+  const addToConfig = (comp: IComponent) => {
+    dispatch(showComponents({ visible: false }));
+
+    if (!config.initConfig.config?.components) {
+      const newConfig: Partial<IConfig> = {
+        config: {
+          components: [comp],
+          price: comp.price,
+        },
+      };
+      dispatch(updateConfig(newConfig));
+      return;
+    }
+    const current = [...config.initConfig.config.components];
+    if (current) {
+      const actualComponent = config.initConfig.config?.components.filter(
+        (component) => component?.type === comp.type
+      );
+      if (actualComponent[0]) {
+        const index = current.indexOf(actualComponent[0]);
+        current.push(comp);
+        current.splice(index);
+
+        const newConfig: Partial<IConfig> = {
+          config: {
+            components: current,
+            price: config.initConfig.price || 0 + comp.price,
+          },
+        };
+
+        dispatch(updateConfig(newConfig));
+
+        return;
+      }
+    }
+
+    console.error("anormal reaction");
+
+    return;
+  };
+
   return (
     <div className="absolute  w-3/5 right-0 h-full bg-slate-400 ">
       <div
@@ -30,21 +73,21 @@ const CompTable = ({
           <h2 className="text-2xl">{components.length} produits compatibles</h2>
         </div>
       </div>
-      <table className="flex flex-col mx-5 space-y-3">
+      <div className="flex flex-col mx-5 space-y-3">
         {components.map((comp, i) => (
-          <td
+          <div
             className="border flex flex-row justify-evenly items-center "
             key={i}
+            onClick={() => addToConfig(comp)}
           >
             <p>{comp.brand}</p>
             <p>{comp.title}</p>
             <p>{comp.socket}</p>
             <p>{comp.price}€</p>
             <p>{comp.rated} stars</p>
-          </td>
+          </div>
         ))}
-        <th className="flex flex-row"></th>
-      </table>
+      </div>
     </div>
   );
 };
